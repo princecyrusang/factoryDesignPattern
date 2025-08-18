1943adcc406477e5aaca6da883eef01f8b0338b0
Prince <115443176+princecyrusang@users.noreply.github.com>
2025-08-18 04:23:25 +0000

    init

---
+++ Clinic.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Clinic {
+    public static void main(String[] args){
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("[1] Dog");
+        System.out.println("[2] Cat");
+        System.out.println("[3] Exit");
+        System.out.print("\nChoose your pet number: ");
+        int choice = input.nextInt();
+
+        PetRecord petFile = new PetRecord();
+        Pet pet;
+
+        switch(choice){
+            case 1: pet = new Dog();
+                petFile.setPetId("D01");
+                petFile.setPetName("Bantay");
+                petFile.setPet(pet);
+                ((Dog) pet).setBreed("German Shepperd");
+                break;
+            case 2: pet = new Cat();
+                petFile.setPetId("C01");
+                petFile.setPetName("Muning");
+                petFile.setPet(pet);
+                ((Cat) pet).setNoOfLives(9);
+                break;
+            case 3: 
+                System.exit(0);
+                break;
+        
+            default: 
+                System.out.println("Invalid choice!");
+        }
+
+        System.out.println("Pet id is " + petFile.getPetId());
+        System.out.println("Pet name is " + petFile.getPetName());
+        System.out.println("Pet kind: " + petFile.getPet().getClass().getSimpleName());
+        System.out.println("Communication sound: "+ petFile.getPet().makeSound());
+        System.out.println("Play mode: " + petFile.getPet().play());
+
+    }
+}
